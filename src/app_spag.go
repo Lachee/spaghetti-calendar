f4a527ea3167adc3378b12c969d7ae60d74d2c38
Lachee <lachlan.h@hotmail.com>
2020-06-26 02:28:57 +1000

    go camera rotation working

--- src/app_spag.go
+++ src/app_spag.go
@@ -26,17 +26,21 @@ func (app *SpagApplication) Update(dt float32) {
 
 	axis := input.GetAxis2D(n.KeyA, n.KeyD, n.KeyS, n.KeyW)
 	vertical := input.GetAxis(n.KeyC, n.KeySpace)
-	app.camera.translate(Vector3{axis.X, vertical, axis.Y})
+	app.camera.translate(Vector3{axis.X, vertical, axis.Y}.Scale(dt * 10))
 
-	rotation := input.GetAxis2D(n.KeyArrowLeft, n.KeyArrowRight, n.KeyArrowDown, n.KeyArrowUp)
-	app.camera.rotate(rotation.ToVector3())
+	//Just testing rotations
+	//rotation := input.GetAxis2D(n.KeyArrowLeft, n.KeyArrowRight, n.KeyArrowDown, n.KeyArrowUp).Scale(90 * n.Deg2Rad * dt)
+	//app.camera.rotate(rotation.ToVector3())
+
+	//Always look at the center
+	app.camera.lookAt(Vector3{0, 0, 0})
 }
 
 //Render draws the frame
 func (app *SpagApplication) Render() {
 	app.renderer.begin()
 
-	transform := n.NewTransform(Vector3{0, 0, 0}, n.NewQuaternionFromEuler(Vector3{0, 0, 0}), Vector3{1, 1, 1})
+	transform := n.NewTransform(Vector3{0, 0, 0}, n.NewQuaternionIdentity(), Vector3{1, 1, 1})
 	app.renderer.draw(&mesh{verts: rotCubeVerts, tris: rotCubeTris}, transform)
 
 	app.renderer.draw(&mesh{verts: rotCubeVerts, tris: rotCubeTris}, app.camera.transform)
--- src/camera.go
+++ src/camera.go
@@ -14,7 +14,7 @@ type camera struct {
 func newCamera() *camera {
 	cam := &camera{}
 	cam.projMatrix = n.NewMatrixPerspective(45.0, float64(n.Width())/float64(n.Height()), 1, 100.0)
-	cam.transform = n.Transform{}
+	cam.transform = n.NewTransformIdentity()
 	cam.transform.Scale = Vector3{0.75, 0.25, 1}.Scale(1)
 	return cam
 }
@@ -26,13 +26,15 @@ func (cam *camera) translate(v Vector3) {
 
 //rotate moves the camera
 func (cam *camera) rotate(v Vector3) {
-	rotation := n.NewQuaternionVector3ToVector3(Vector3{0, 1, 0}, v)
-	cam.transform.Rotation = n.NewQuaternionFromMatrix(rotation.ToMatrix())
 
-	//euler := cam.transform.Rotation.ToEuler()
-	//euler = euler.Add(v)
-	//cam.transform.Rotation = n.NewQuaternionVector3ToVector3(Vector3{0, 1, 0}, euler)
-	//cam.transform.Rotation = n.NewQuaternionLookRotation(v, Vector3{0, 1, 0})
+	euler := n.NewQuaternionEuler(v)
+	rotation := euler.Multiply(cam.transform.Rotation)
+	cam.transform.Rotation = rotation
+}
+
+//lookAt forces the camera to look at a point
+func (cam *camera) lookAt(v Vector3) {
+	cam.transform.Rotation = n.NewQuaternionLookAt(cam.transform.Position, v, Vector3{0, 1, 0})
 }
 
 //view returns the view matrix
